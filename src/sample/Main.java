5509d145517dc1de730df6e09e2f9ea4c7a48c0e
BT_1N3_30 <dwong0023@bths.edu>
2019-02-06 10:46:37 -0500

    plannedout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -31,7 +31,7 @@ public class Main extends Application {
         button1.setOnAction(event -> primaryStage.setScene(scene2));
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1, button1);
-        scene1= new Scene(layout1, 300, 250);
+        scene1= new Scene(layout1, 1000, 800);
         layout1.setId("s1");
 
 //Scene 2
@@ -40,7 +40,7 @@ public class Main extends Application {
         button2.setOnAction(event -> primaryStage.setScene(scene3));
         VBox layout2= new VBox(20);
         layout2.getChildren().addAll(label2, button2);
-        scene2= new Scene(layout2,300,250);
+        scene2= new Scene(layout2,1000,800);
         layout2.setId("s2");
 
 //Scene 3
@@ -49,7 +49,7 @@ public class Main extends Application {
         button3.setOnAction(event -> primaryStage.setScene(scene4));
         VBox layout3 = new VBox(20);
         layout3.getChildren().addAll(label3, button3);
-        scene3= new Scene(layout3,300,250);
+        scene3= new Scene(layout3,1000,800);
         layout3.setId("s3");
 
         //Scene 4
@@ -58,7 +58,7 @@ public class Main extends Application {
         button4.setOnAction(event -> primaryStage.setScene(scene1));
         VBox layout4 = new VBox(20);
         layout4.getChildren().addAll(label4, button4);
-        scene4= new Scene(layout4,300,250);
+        scene4= new Scene(layout4,1000,800);
         layout4.setId("s4");
 
         layout1.getStylesheets().add("/sample/bottlestyle.css");
@@ -89,4 +89,11 @@ public class Main extends Application {
     //VBox layout1 = new VBox(20); This reopens the virtual box, but it keeps it at the same size
     //layout1.getChildren().addAll(label1, button1); This function adds the label and button as children of the virtual box
     //scene1= new Scene(layout1, 300, 250); This initializes the first scene. The first scene is the layout with its children and the size is set up.
+    //for the stylesheet to work you gotta add it to the layout and set ids for the things you make here
+    //whatamidoingnow i should start with the third scene, the main game, and make buttons with images of bottles. The buttons will have to work.
+    //plan - first scene title, second instructions, third main game, fourth bottle main game, fifth level results, sixth end game??
+    //either way i can just work on the main game, THEN set it permanently to a scene. Scene for now will be scene #2.
+    //now how in the world do i do this? first - make the buttons and place them second make them do something.
+    //
+
 }
