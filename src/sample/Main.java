484fa229aff8a5c77eb5babec6c089852ddd195a
BT_1N3_30 <dwong0023@bths.edu>
2019-02-06 10:37:37 -0500

    bgfixed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,18 +20,19 @@ public class Main extends Application {
 
     Scene scene1, scene2, scene3, scene4;
 
-
+//add the police song message in a bottle or one of the instrumentals or both
     @Override
     public void start(Stage primaryStage)throws Exception {
         primaryStage.setTitle("SceneTest");
+
 //Scene 1
         Label label1= new Label("This is the first scene");
         Button button1= new Button("Go to scene 2");
         button1.setOnAction(event -> primaryStage.setScene(scene2));
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1, button1);
-        layout1.getStylesheets().add("/sample/bottlestyle.css");
         scene1= new Scene(layout1, 300, 250);
+        layout1.setId("s1");
 
 //Scene 2
         Label label2= new Label("This is the second scene");
@@ -40,6 +41,7 @@ public class Main extends Application {
         VBox layout2= new VBox(20);
         layout2.getChildren().addAll(label2, button2);
         scene2= new Scene(layout2,300,250);
+        layout2.setId("s2");
 
 //Scene 3
         Label label3 = new Label("This is the third scene");
@@ -48,6 +50,7 @@ public class Main extends Application {
         VBox layout3 = new VBox(20);
         layout3.getChildren().addAll(label3, button3);
         scene3= new Scene(layout3,300,250);
+        layout3.setId("s3");
 
         //Scene 4
         Label label4 = new Label("This is the fourth scene");
@@ -56,6 +59,12 @@ public class Main extends Application {
         VBox layout4 = new VBox(20);
         layout4.getChildren().addAll(label4, button4);
         scene4= new Scene(layout4,300,250);
+        layout4.setId("s4");
+
+        layout1.getStylesheets().add("/sample/bottlestyle.css");
+        layout2.getStylesheets().add("/sample/bottlestyle.css");
+        layout3.getStylesheets().add("/sample/bottlestyle.css");
+        layout4.getStylesheets().add("/sample/bottlestyle.css");
 
         primaryStage.setScene(scene1);
         primaryStage.show();
