80e656a095a419e7f6f465c548f97d09c9b62d92
BT_1N3_30 <dwong0023@bths.edu>
2019-02-06 11:44:23 -0500

    changingbuttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -31,16 +31,38 @@ public class Main extends Application {
         button1.setOnAction(event -> primaryStage.setScene(scene2));
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1, button1);
-        scene1= new Scene(layout1, 1000, 800);
+        scene1= new Scene(layout1, 1200, 800);
         layout1.setId("s1");
 
 //Scene 2
         Label label2= new Label("This is the second scene");
         Button button2= new Button("Go to scene 3");
         button2.setOnAction(event -> primaryStage.setScene(scene3));
+
+        //MAINGAMESETUP
+        Button bot1= new Button("BOTTLE1");
+        Button bot2= new Button("BOTTLE2");
+        Button bot3= new Button("BOTTLE3");
+        Button bot4= new Button("BOTTLE4");
+        Button bot5= new Button("BOTTLE5");
+        Button bot6= new Button("BOTTLE6");
+
+        bot1.setId("b1");
+
+        bot2.setId("b2");
+        bot3.setId("b3");
+        bot4.setId("b4");
+        bot5.setId("b5");
+        bot6.setId("b6");
+
+        //ENDOFMAINGAMESETUP
+        bot1.setLayoutX(400);
+        bot1.setLayoutY(400);
+
         VBox layout2= new VBox(20);
-        layout2.getChildren().addAll(label2, button2);
-        scene2= new Scene(layout2,1000,800);
+        layout2.getChildren().addAll(label2, button2, bot1, bot2, bot3, bot4, bot5, bot6);
+
+        scene2= new Scene(layout2,1200,800);
         layout2.setId("s2");
 
 //Scene 3
@@ -49,7 +71,7 @@ public class Main extends Application {
         button3.setOnAction(event -> primaryStage.setScene(scene4));
         VBox layout3 = new VBox(20);
         layout3.getChildren().addAll(label3, button3);
-        scene3= new Scene(layout3,1000,800);
+        scene3= new Scene(layout3,1200,800);
         layout3.setId("s3");
 
         //Scene 4
@@ -58,7 +80,7 @@ public class Main extends Application {
         button4.setOnAction(event -> primaryStage.setScene(scene1));
         VBox layout4 = new VBox(20);
         layout4.getChildren().addAll(label4, button4);
-        scene4= new Scene(layout4,1000,800);
+        scene4= new Scene(layout4,1200,800);
         layout4.setId("s4");
 
         layout1.getStylesheets().add("/sample/bottlestyle.css");
